7ff1c9df4570d725d9efd76af39f06cb7ef81ae1
yaho86 <yaser.hoseini86@gmail.com>
2023-11-04 15:54:15 +0330

    spell

--- app/component/AddTodo.jsx
+++ app/component/AddTodo.jsx
@@ -45,7 +45,7 @@ function AddTodo() {
         //     .post("https://rahanik.iran.liara.run/add", { text: input })
         //     .then((res) => console.log(res.data))
       >
-        add
+        <Add></Add>
       </button>
     </>
   );
