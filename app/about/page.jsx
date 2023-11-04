0dd23d32f35d0c07425b2a6790bc378f709b6d1b
yaho86 <yaser.hoseini86@gmail.com>
2023-11-04 21:15:40 +0330

    change text

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -11,21 +11,21 @@ function About() {
             "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
         }}
       >
-        <div className="hero-overlay bg-opacity-90"></div>
+        <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content ">
           <div className="max-w-2xl">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <h1 className="mb-5 text-5xl font-bold">Dear visitor</h1>
             <p className="mb-5 text-xl text-justify ">
               This app is developed by{" "}
               <span className="text-yellow-500">Enger Yaser Hoseini</span> and{" "}
               <span className="text-yellow-500">Enger Reza Hadipour</span> in
-              fact front-end is developed by Enger Yaser Hosein and back-end is
-              developed by Enger Reza Hadipour , in home page you are able to
-              add task or todos and after add the todo will be added to mongodb
-              database and the data is added to state after that data will be
-              shared in list with context Provider duo to react context every
-              thing can add delete and update without and delay and page
-              don&apos;t need to refresh for showing new entries
+              fact front-end is developed by Yaser Hosein and back-end is
+              developed by Reza Hadipour , in home page you are able to add task
+              or todos and after add the todo will be added to mongodb database
+              and the data is added to state after that data will be shared in
+              list with context Provider duo to react context every thing can
+              add , delete and update without any delay and page don&apos;t need
+              to refresh for showing new entries
             </p>
             <Link href={"/"}>
               <button className="btn btn-primary">Get Started</button>
