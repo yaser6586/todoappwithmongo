dbf4daa8c23dada5b5833ed13f6f68df85815140
yaho86 <yaser.hoseini86@gmail.com>
2023-11-04 21:30:59 +0330

    change background

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -7,11 +7,10 @@ function About() {
       <div
         className="hero min-h-screen"
         style={{
-          backgroundImage:
-            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+          backgroundImage: "url(tech1.jpg)",
         }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-overlay bg-opacity-90"></div>
         <div className="hero-content text-center text-neutral-content ">
           <div className="max-w-2xl">
             <h1 className="mb-5 text-5xl font-bold">Dear visitor</h1>
