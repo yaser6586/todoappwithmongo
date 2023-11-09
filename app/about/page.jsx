e3f5c2e85655dd379adb241f077c7c88f3d50f2f
yaho86 <yaser.hoseini86@gmail.com>
2023-11-09 18:45:10 +0330

    first commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -23,15 +23,13 @@ function About() {
             <h1 className="mb-5 text-5xl font-bold">Dear visitor</h1>
             <p className="mb-5 text-xl text-justify ">
               This app is developed by{" "}
-              <span className="text-yellow-500">Enger Yaser Hoseini</span> and{" "}
-              <span className="text-yellow-500">Enger Reza Hadipour</span> in
-              fact front-end is developed by Yaser Hosein and back-end is
-              developed by Reza Hadipour , in home page you are able to add task
-              or todos and after add the todo will be added to mongodb database
-              and the data is added to state after that data will be shared in
-              list with context Provider duo to react context every thing can
-              add , delete and update without any delay and page don&apos;t need
-              to refresh for showing new entries
+              <span className="text-yellow-500">Enger Yaser Hoseini</span> , in
+              home page you are able to add task or todos and after add the todo
+              will be added to mongodb database and the data is added to state
+              after that data will be shared in list with context Provider duo
+              to react context every thing can add , delete and update without
+              any delay and page don&apos;t need to refresh for showing new
+              entries
             </p>
             <Link href={"/"}>
               <button className="btn btn-primary">Get Started</button>
